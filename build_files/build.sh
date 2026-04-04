f660be931bdeebae3b2d9b403aa3f15fc2f56b81
dalmatinos <dalmatinos@hotmail.com>
2026-04-04 09:38:47 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,12 +8,9 @@ set -ouex pipefail
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-# enable rpmfusion repos and add copr repos
-dnf config-manager setopt rpmfusion-nonfree-steam.enabled=1
 
 # this installs a package from fedora repos
-dnf5 install -y alacritty \
-                steam
+dnf5 install -y alacritty 
 # this removes unwanted software
 dnf5 remove -y Sunshine 
 
